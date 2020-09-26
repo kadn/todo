e4ed52cef11e94c864fe5b079cbaeae6348d00de
kadn <826363145@qq.com>
2020-09-26 23:04:06 +0800

    为completed添加了更好的样式，同时能够编辑事件

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,6 +4,10 @@ var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem(
   completed: []
 };
 
+oldcontent = '';
+currentindex = 0;
+newcontent = '';
+
 // Remove and complete icons in SVG format
 var removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
 var completeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
@@ -52,11 +56,27 @@ function dataObjectUpdated() {
   localStorage.setItem('todoList', JSON.stringify(data));
 }
 
+function recordValue() {
+  oldcontent = this.value;
+  currentindex = data.completed.indexOf(oldcontent);
+  console.log(currentindex);
+  console.log(oldcontent);
+  console.log(data);
+}
+
+function changeItem() {
+  newcontent = this.value;
+  data.completed[currentindex] = newcontent;
+  console.log(newcontent);
+  console.log(data);
+  dataObjectUpdated();
+}
+
 function removeItem() {
   var item = this.parentNode.parentNode;
   var parent = item.parentNode;
   var id = parent.id;
-  var value = item.innerText;
+  var value = item.childNodes[0].value;
 
   if (id === 'todo') {
     data.todo.splice(data.todo.indexOf(value), 1);
@@ -72,7 +92,7 @@ function completeItem() {
   var item = this.parentNode.parentNode;
   var parent = item.parentNode;
   var id = parent.id;
-  var value = item.innerText;
+  var value = item.childNodes[0].value;
 
   if (id === 'todo') {
     data.todo.splice(data.todo.indexOf(value), 1);
@@ -95,7 +115,15 @@ function addItemToDOM(text, completed) {
   var list = (completed) ? document.getElementById('completed'):document.getElementById('todo');
 
   var item = document.createElement('li');
-  item.innerText = text;
+  // item.innerText = text;
+  // 使用input代替纯文本
+  var inputElement = document.createElement('input');
+  inputElement.value = text;
+  // input 事件，每次输入一个字，就会触发
+  // change 事件，会在该input失去焦点时触发，很合适
+  // focues 事件，当鼠标点击过来时，就会触发
+  inputElement.addEventListener('focus', recordValue);
+  inputElement.addEventListener('change', changeItem);
 
   var buttons = document.createElement('div');
   buttons.classList.add('buttons');
@@ -116,6 +144,7 @@ function addItemToDOM(text, completed) {
 
   buttons.appendChild(remove);
   buttons.appendChild(complete);
+  item.appendChild(inputElement);
   item.appendChild(buttons);
 
   list.insertBefore(item, list.childNodes[0]);
